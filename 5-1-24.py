ae0f647653b0f543ada94dd152f050a28195b9de
santoshred524 <46974840+santoshred524@users.noreply.github.com>
2024-05-01 13:32:00 -0500

    Update 5-1-24.py

--- 5-1-24.py
+++ 5-1-24.py
@@ -4,3 +4,12 @@ Traceback (most recent call last):
     send_alert(event, account_alias, f"Unauthorized access detected in {bucket_name} in account {account_alias}")
   File "/var/task/functions.py", line 36, in send_alert
     for statement in parsed_data["Statement"]:
+
+
+
+[ERROR] TypeError: 'NoneType' object is not subscriptable
+Traceback (most recent call last):
+  File "/var/task/s3_policy.py", line 22, in lambda_handler
+    send_alert(event, bucket_policy, f"Unauthorized access detected in {bucket_name} in account {account_alias}")
+  File "/var/task/functions.py", line 36, in send_alert
+    for statement in parsed_data["Statement"]:
