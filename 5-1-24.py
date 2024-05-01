77139133390843610a7966dafa95e331715878be
santoshred524 <46974840+santoshred524@users.noreply.github.com>
2024-05-01 13:29:31 -0500

    Create 5-1-24.py

---
+++ 5-1-24.py
@@ -0,0 +1,6 @@
+[ERROR] TypeError: string indices must be integers, not 'str'
+Traceback (most recent call last):
+  File "/var/task/s3_policy.py", line 22, in lambda_handler
+    send_alert(event, account_alias, f"Unauthorized access detected in {bucket_name} in account {account_alias}")
+  File "/var/task/functions.py", line 36, in send_alert
+    for statement in parsed_data["Statement"]:
